d53ef84f3dd0ddf224045f64bc557199b0f2da67
Lebot Apps <lebot.llc@gmail.com>
2021-06-06 13:05:52 -0400

    updated mvc & index page

--- server.js
+++ server.js
@@ -23,6 +23,63 @@ app.get('/pokemon', (req, res)=>{
         });
     });
 
+    // NEW
+app.get("/pokemon/new", (req, res) => {
+    res.render('new.ejs');
+  });
+  
+  // CREATE
+  app.post("/pokemon", (req, res) => {
+    // if (req.body.readyToEat === "on"){
+    //   req.body.readyToEat = true;
+    // } else {
+    //   req.body.readyToEat = false;
+    // }
+    Pokemon.push(req.body);
+    console.log(Pokemon);
+    res.redirect("/pokemon");
+  });
+  
+  // SHOW
+  app.get('/pokemon/:indexOfPokemonArray', (req, res) => {
+    res.render('show.ejs', { //second param must be an object
+        Pokemon: Pokemon[req.params.indexOfPokemonArray] //there will be a variable available inside the ejs file called fruit, its value is fruits[req.params.indexOfFruitsArray]
+    });
+  });    
+  
+  // Products route for CURL lesson
+  app.post('/products', (req, res)=>{
+    console.log('Create route accessed!');
+    console.log('Req.body is: ', req.body);
+    res.send(req.body);
+  });
+  
+  // DELETE route 
+  
+  app.delete('/pokemon/:indexOfFruitsArray', (req, res) =>{
+    fruits.splice(req.params.indexOfPokemonArray, 1);
+    res.redirect('/pokemon');
+  })
+  
+  // EDIT 
+  app.get('/pokemon/:indexOfPokemonArray/edit', (req, res)=>{
+    res.render('edit.ejs', {
+      Pokemon: Pokemon[req.params.indexOfPokemonArray],
+      index: req.params.indexOfPokemonArray
+    });
+  });
+  
+  //UPDATE
+  
+  
+    app.put('/pokemon/:indexOfPokemonArray', (req, res) => { 
+      
+      
+      Pokemon[req.params.indexOfPokemonArray] = req.body; 
+      res.redirect('/pokemon'); 
+    });
+  
+
 //Listening Port 
 
 app.listen(3000);
